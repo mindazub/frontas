c3658a1aa34e88ab6dacc6a82b68df008e30a7b8
Mindaugas Ažubalis <mindaugas.azubalis@gmail.com>
2017-03-18 10:24:25 +0200

    2nd commit with all initial project completed

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
 gulp.task('connect', function() {
     connect.server({
@@ -8,11 +10,25 @@ gulp.task('connect', function() {
 });
 
 gulp.task('html', function() {
-    gulp.src('./web/*.html');
+    gulp.src('./web/*.html')
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./web/assets'));
+});
+
+gulp.task('js', function() {
+  return gulp.src('./src/js/**/*.js')
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('./web/assets'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch(['web/*.html'], ['html']);
+    gulp.watch(['./web/*.html'], ['html']);
+    gulp.watch('./src/sass/**/*.scss', ['sass']);
+    gulp.watch('./src/js/**/*.js', ['js']);
 });
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', ['connect', 'sass', 'js', 'watch']);
